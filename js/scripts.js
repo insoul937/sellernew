217acd5a78de41e9dbdab9b6948072404ae89dab
insoul937 <31006119+insoul937@users.noreply.github.com>
2019-10-11 17:12:52 +0500

    catalog 1920

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 var $ = jQuery;
 
 $(document).ready(function () {
+    $('.see-ul').on('click',function () {
+       $(this).parent().find('.ls-hidden').slideToggle();
+       return false;
+    });
     $('.top-predl').owlCarousel({
         items: 6,
         smartSpeed: 800,
@@ -8,6 +12,12 @@ $(document).ready(function () {
         nav: true,
         navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
         dots: false,
+        autoplay: true,
+    });
+    $('.top-slider').owlCarousel({
+        items: 1,
+        smartSpeed: 800,
+        dots: true,
         autoplay: true,
     });
     $('.product-carousel').owlCarousel({
